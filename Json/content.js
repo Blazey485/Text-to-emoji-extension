6450084662c8a8687e20103ee0a1027537ca8e9a
Blazey485 <niksandhakal@gmail.com>
2026-06-22 13:08:53 +0200

    removed the testing consolelogs

--- Json/content.js
+++ Json/content.js
@@ -1,4 +1,1 @@
 console.log("test 1 check check if text to emoji working");
-console.log("test 1 check check if text to emoji working");
-console.log("test 1 check check if text to emoji working");
-console.log("test 1 check check if text to emoji working");
